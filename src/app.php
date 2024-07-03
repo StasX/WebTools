3625e5ee506ecf9cac815577d905155d70217287
StasX <s.mestechkin@gmail.com>
2024-07-03 22:44:08 +0300

    Init commit

---
+++ src/app.php
@@ -0,0 +1,7 @@
+<?php
+use Slim\Factory\AppFactory;
+
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
---
+++ src/utils/DAL.php
@@ -0,0 +1,18 @@
+<?php
+class DAL
+{
+    public function __construct()
+    {
+
+    }
+    public function get_table()
+    {}
+    public function get_scalar($id)
+    {}
+    public function insert()
+    {}
+    public function update()
+    {}
+    public function delete($id)
+    {}
+}
