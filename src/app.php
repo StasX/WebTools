151b9687ac625d7a66ed48c4157fbf758957af8a
StasX <s.mestechkin@gmail.com>
2024-07-04 22:59:59 +0300

    Add mustache engine to container

--- src/app.php
+++ src/app.php
@@ -1,7 +1,14 @@
 <?php
-use Slim\Factory\AppFactory;
 
+use DI\Container as Container;
+use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$container = new Container();
+$mustache = new Mustache_Engine(array('entity_flags' => ENT_QUOTES));
+$container->set("mustache", $mustache);
+
+AppFactory::setContainer($container);
 $app = AppFactory::create();
+require_once __DIR__ . "/routes/auth.php";
